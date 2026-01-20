b96598e021b8888c194f0dedbe8b37ff439f3d9d
JulianBohne <julianbohnenkaemper@gmail.com>
2026-01-20 23:07:37 +0100

    Quicksave

--- build-slang.bat
+++ build-slang.bat
@@ -2,5 +2,7 @@
 
 echo Compiling slang to wgsl
 @REM call slangc slang/test-compute.slang -target wgsl -o wgsl/compiled-test-compute.wgsl || exit /b 1
-call slangc slang/matmul.slang -target wgsl -O3 -o wgsl/compiled-matmul.wgsl || exit /b 1
+@REM call slangc slang/matmul.slang -target wgsl -O3 -o wgsl/compiled-matmul.wgsl || exit /b 1
+call slangc slang/splat.slang -entry vert -target wgsl -o wgsl/compiled-splat-vertex.wgsl || exit /b 1
+call slangc slang/splat.slang -entry frag -target wgsl -o wgsl/compiled-splat-fragment.wgsl || exit /b 1
 echo Success!
